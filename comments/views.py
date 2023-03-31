ea042317bc87a880c5cf1e27eeaf1a2969da6771
Amanda Carpio <amandamarielc@gmail.com>
2023-03-31 01:01:26 -0400

    final edits on put request done

--- comments/views.py
+++ comments/views.py
@@ -22,7 +22,7 @@ class CommentViewSet(viewsets.ModelViewSet):
 
         return Response([], status=status.HTTP_200_OK)    
 
-    @action(methods=['GET', 'PUT'], detail=True)
+    @action(methods=['GET'], detail=True)
     def individual_review(self, request, pk=None):
         queryset = Comment.objects.filter(id = pk)
         serializer = self.get_serializer(queryset, many=True)
@@ -30,10 +30,27 @@ class CommentViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response([], status=status.HTTP_200_OK)
     
-    # @action(methods=['PUT'], detail=True)
-    # def individual_review(self, request, pk=None):
-    #     queryset = Comment.objects.filter(id = pk)
-    #     serializer = self.get_serializer(queryset, many=True)
-    #     if serializer.data:
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-    #     return Response([], status=status.HTTP_200_OK)
+    @action(methods=['PUT'], detail=True)
+    def individual_review(self, request, pk=None):
+        print(self.get_object())
+        instance=self.get_object()
+        data={
+            "name": request.data.get("name"),
+            "city": request.data.get("city"),
+            "rating": request.data.get("rating"),
+            "review": request.data.get("review"),
+            "yelpId": request.data.get("yelpId")
+        }
+        serializer = self.serializer_class(instance=instance,
+            data=data, # or request.data
+            partial=True)
+        # queryset = Comment.objects.filter(id = pk)
+        # serializer = self.get_serializer(queryset)
+        # if serializer.data:
+        #     return Response(serializer.data, status=status.HTTP_200_OK)
+        # return Response([], status=status.HTTP_200_OK)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
