ba643f359fa4b6583ed3c97f3046110559a4f6b1
Amanda Carpio <amandamarielc@gmail.com>
2023-03-31 00:19:49 -0400

    more edits

--- comments/views.py
+++ comments/views.py
@@ -24,8 +24,6 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     @action(methods=['GET', 'PUT'], detail=True)
     def individual_review(self, request, pk=None):
-        if(request.method == 'PUT'): 
-            return Response([request], status=status.HTTP_200_OK)  
         queryset = Comment.objects.filter(id = pk)
         serializer = self.get_serializer(queryset, many=True)
         if serializer.data:
